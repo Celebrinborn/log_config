97da1e2076b1305293ffcec97966fa3a2b43547a
Celebrinborn <cameronjunkmail1@gmail.com>
2023-04-01 19:46:43 -0700

    coppied mcoding layout

--- setup.py
+++ setup.py
@@ -1,17 +1,4 @@
 from setuptools import setup
 
-setup(
-    name='log_config',
-    version='0.1',
-    packages=['log_config_package'],
-    install_requires=[
-        # List any dependencies your package requires
-        'logging',
-        'pyyaml'
-    ],
-    url='https://github.com/Celebrinborn/log_config',
-    license='',
-    author='Celebrinborn',
-    author_email='na',
-    description='A package for configuring logging'
-)
+if __name__ == "__main__":
+    setup()
