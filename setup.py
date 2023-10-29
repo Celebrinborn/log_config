0fd6f96a11919de7dd53faa81e84d2f2ebf7c900
Celebrinborn <cameronjunkmail1@gmail.com>
2023-10-29 21:49:28 +0000

    this should let this work?

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-if __name__ == "__main__":
-    setup()
+setup(
+    name='log_config',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    url='https://github.com/Celebrinborn/log_config',
+    author='Celebrinborn',
+    author_email='your-email@example.com',
+    description='An easy log setup script',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+)
